1b0112e0ec06268d7ff5a2eb952d63c881edc90e
alex-kunev <69144667+alex-kunev@users.noreply.github.com>
2021-11-30 14:44:56 +0200

    10/30 exercices

---
+++ methods/printTriangle.java
@@ -0,0 +1,49 @@
+package methods;
+
+import java.util.Scanner;
+
+public class printTriangle {
+    //Create a method for printing triangles as shown below: 
+    //1 
+    //1 2
+    //1 2 3
+    //1 2
+    //1 
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+
+        printAll(length);
+        
+        
+
+    }
+
+    //Single line print: 
+    static void printLine(int start, int end) {
+        for (int i = start; i < end; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    //Side Triangle print:
+    static void printAll(int length) {
+        for (int i = 1; i <= length; i++) {
+            for (int j = 1; j<= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for (int i = length-1; i > 0; i--) {
+            for(int j = 1; j <=i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
---
+++ methods/rectangleArea.java
@@ -0,0 +1,20 @@
+package methods;
+
+import java.util.Scanner;
+
+public class rectangleArea {
+    //Create a method which returns rectangle area with given width and height
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int width = sc.nextInt();
+        int height = sc.nextInt();
+        rectangleArea(width, height);
+        
+    }
+
+    private static int rectangleArea(int width, int height) {
+        System.out.println("Width is: " + width);
+        System.out.println("Height is: " + height);
+        return width*height;
+    }
+}
